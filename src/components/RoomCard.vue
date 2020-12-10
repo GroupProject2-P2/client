2ecd6dd77338dbc29033186dac06fb580e735255
hafis915 <hafistafsani915uh@gmail.com>
2020-12-11 02:27:52 +0800

    tambahan view/roomDasboard

---
+++ src/components/RoomCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card col-sm-4" style="width: 18rem;">
+        <div class="card-header">
+            <h1> Nama room</h1>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Player:</h5>
+            <ul>
+                <li>Player 1</li>
+                <li>Player 2</li>
+                <li>Player 3</li>
+                <li>Player 4</li>
+            </ul>
+
+            <button class="btn btn-outline-primary container-fluid">Join</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'RoomCard'
+}
+</script>
+
+<style>
+
+</style>
