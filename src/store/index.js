1b6cd1cadc46ecea2befe3f72a010a7febcf2c9b
hafis915 <hafistafsani915uh@gmail.com>
2020-12-11 01:23:21 +0800

    buat tampilan login di views/home

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    joinUsers: [],
+    welcomeMessage: ''
   },
   mutations: {
+    SOCKET_welcomeMessage (state,payload) {
+      state.welcomeMessage = payload
+    },
+    addNewUserLogin (state, payload) {
+      state.usersLogin.push(payload)
+    },
+    SOCKET_login (state,payload) {
+      console.log(payload,'store')
+      state.joinUsers.push(payload)
+    }
   },
   actions: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container-login card">
+        <div class="card-header">
+            <h1>{{welcomeMessage}}</h1>
+        </div>
+        <div class="card-body">
+            <form action="">
+                <div class="email mb-3">
+                    <h5>Insert Your Name</h5>
+                    <input type="text" name="text" class="container-fluid">
+                </div>
+                <div class="button-login row">
+                    <button class="btn btn-primary mt-3">Lets Play</button>
+                    <div id="emailHelp" class="form-text">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      userName: ''
+    }
+  },
+  methods: {
+    insertUser () {
+      this.$socket.emit('login',this.userName)
+    }
+  },
+  computed: {
+    ...mapState ({
+      welcomeMessage: 'welcomeMessage'
+    })
   }
+
 }
 </script>
